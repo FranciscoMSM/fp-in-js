22958b06d6a424d44f4a849f5f10c85f49fabdc8
Francisco Magalhães <franciscomags@gmail.com>
2018-04-21 09:59:47 +0100

    updated function composition

--- func-composition.js
+++ func-composition.js
@@ -1,20 +1,17 @@
-const toSlug = input =>
-  encodeURIComponent(
-    input
-      .split(' ')
-      .map(str => str.toLowerCase())
-      .join('-'),
-  );
+function multBy2(a) {
+  return a * 2;
+}
 
-const toSlug = input =>
-  encodeURIComponent(join('-')(map(toLowerCase)(split(' ')(input))));
+function add2(a) {
+  return a + 2;
+}
+
+console.log([1, 2, 3].map(multBy2)); // [2, 4, 6]
+console.log([1, 2, 3].map(multBy2).map(add2)); // [4, 6, 8]
+
+const multBy2Add2 = compose(multBy2, add2);
+
+console.log([1, 2, 3].map(multiplyBy2Add2)); // [4, 6, 8]
 
 const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
-
-const toSlug = compose(
-  encodeURIComponent,
-  join('-'),
-  map(toLowerCase),
-  split(' '),
-);
